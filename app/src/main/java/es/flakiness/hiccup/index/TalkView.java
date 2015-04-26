ac5f9edba32861de75b5c5d31d5a0a8647355848
Hajime Morrita <omo@dodgson.org>
2015-04-25 21:39:08 -0700

    Get rid of CardView. Fix #21
    Needs some cleanup.

--- app/src/main/java/es/flakiness/hiccup/index/TalkView.java
+++ app/src/main/java/es/flakiness/hiccup/index/TalkView.java
@@ -2,17 +2,16 @@ package es.flakiness.hiccup.index;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
-import es.flakiness.hiccup.Injections;
 import es.flakiness.hiccup.R;
 
-public class TalkView extends CardView {
+public class TalkView extends FrameLayout {
 
     @InjectView(R.id.talk_title) TextView title;
     @InjectView(R.id.talk_duration) TextView duration;
@@ -33,7 +32,6 @@ public class TalkView extends CardView {
     private void initialize() {
         LayoutInflater.from(getContext()).inflate(R.layout.talk_view, this);
         ButterKnife.inject(this);
-        setContentPadding(10, 10, 10, 10);
    }
 
     public void setPreso(TalkPreso preso) {
@@ -44,7 +42,7 @@ public class TalkView extends CardView {
     @Override
     public void setActivated(boolean selected) {
         super.setActivated(selected);
-        int color = isActivated() ? Color.rgb(0xdd, 0xdd, 0xff) : Color.WHITE;
-        setCardBackgroundColor(color);
+        int color = isActivated() ? Color.rgb(0xdd, 0xdd, 0xff) : Color.TRANSPARENT;
+        setBackgroundColor(color);
     }
 }
